ef1ea5263658766074f1c3977e56acf4f0a461c1
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2024-02-17 21:23:14 +0300

    Update binaryNumbers.cpp

--- 1lab/binaryNumbers.cpp
+++ 1lab/binaryNumbers.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include"Header.h"
-vector<int> convert_straight(int Number) {
+vector<int> convertStraight(int Number) {
 	vector<int> binary;
 	if (Number < 0) {
 		binary.push_back(1);
@@ -16,85 +16,96 @@ vector<int> convert_straight(int Number) {
 	}
 	return binary;
 }
-vector<int> convert_inverse(int Number) {
-	vector<int> binary = convert_straight(Number);
-	if (binary[0] == 1 || binary[0] == 0) {
-		for (size_t i = 1; i < binary.size(); ++i) {
-			binary[i] = (binary[i] == 0) ? 1 : 0;
-		}
+vector<int> convertInverse(int Number) {
+	vector<int> binary = convertStraight(Number);
+	if (binary[0] == 0) {
+		return binary;
 	}
-	return binary;
-}
-vector<int> convert_dop(int Number) {
-	vector<int> binary = convert_inverse(Number);
-	binary[0] = 1;
-	int c = 1;
-	for (int i = binary.size() - 1; i > 0; --i) {
-		int sum = binary[i] + c;
-		binary[i] = sum % 2;
-		c = sum / 2;
-		if (c == 0) {
-			break;
+	else {
+		if (binary[0] == 1 || binary[0] == 0) {
+			for (size_t i = 1; i < binary.size(); ++i) {
+				binary[i] = (binary[i] == 0) ? 1 : 0;
+			}
 		}
+		return binary;
 	}
-	return binary;
 }
-vector<int> addition(const vector<int>& a, const vector<int>& b) {
+vector<int> convertDop(int Number) {
+	vector<int> binary = convertStraight(Number);
+	if (binary[0] == 0) {
+		return binary;
+	}
+	else {
+		vector<int> binary = convertInverse(Number);
+		binary[0] = 1;
+		int carry = 1;
+		for (int i = binary.size() - 1; i > 0; --i) {
+			int sum = binary[i] + carry;
+			binary[i] = sum % 2;
+			carry = sum / 2;
+			if (carry == 0) {
+				break;
+			}
+		}
+		return binary;
+	}
+}
+vector<int> addition(const vector<int>& firstNumber, const vector<int>& secondNumber) {
 	vector<int> result;
-	int c = 0;
-	for (int i = a.size() - 1; i >= 0; --i) {
-		int sum = a[i] + b[i] + c;
+	int carry = 0;
+	for (int i = firstNumber.size() - 1; i >= 0; --i) {
+		int sum = firstNumber[i] + secondNumber[i] + carry;
 		result.insert(result.begin(), sum % 2);
-		c = sum / 2;
+		carry = sum / 2;
 	}
-	if (c > 0) {
-		result.insert(result.begin(), c);
+	if (carry > 0) {
+		result.insert(result.begin(), carry);
 	}
 	return result;
 }
-vector<int> substraction(const vector<int>& a, const vector<int>& b) {
+vector<int> substraction(const vector<int>& firstNumber, const vector<int>& secondNumber) {
 	vector<int> result;
-	int c = 0;
-	for (int i = a.size() - 1; i >= 0; --i) {
-		int diff = a[i] - b[i] - c;
+	int carry = 0;
+	for (int i = firstNumber.size() - 1; i >= 0; --i) {
+		int diff = firstNumber[i] - secondNumber[i] - carry;
 		if (diff < 0) {
 			diff += 2;
-			c = 1;
+			carry = 1;
 		}
 		else {
-			c = 0;
+			carry = 0;
 		}
 		result.insert(result.begin(), diff);
 	}
 	return result;
 }
-vector<int> complement(const vector<int>& a) {
+vector<int> complement(const vector<int>& firstNumber) {
 	vector<int> result;
-	for (int bit : a) {
+	for (int bit : firstNumber) {
 		result.push_back(bit == 0 ? 1 : 0);
 	}
 	return result;
 }
-vector<int> increment(const vector<int>& a) {
-	vector<int> result = a;
+vector<int> increment(const vector<int>& firstNumber) {
+	vector<int> result = firstNumber;
 	int carry = 1;
-	for (int i = a.size() - 1; i >= 0 && carry; --i) {
+	for (int i = firstNumber.size() - 1; i >= 0 && carry; --i) {
 		result[i] += carry;
 		carry = result[i] / 2;
 		result[i] %= 2;
 	}
 	return result;
 }
-int getSign(const vector<int>& a) {
-	return a.front();
+int getSign(const vector<int>& firstNumber) {
+	return firstNumber.front();
 }
-vector<int> multiplication(const vector<int>& a, const vector<int>& b) {
-	int size = a.size();
+vector<int> multiplication(const vector<int>& firstNumber, const vector<int>& secondNumber) {
+	int size = firstNumber.size();
 	vector<int> result(size * 2, 0);
-	int sign_a = getSign(a);
-	int sign_b = getSign(b);
-	vector<int> a_abs = (sign_a == 1) ? increment(complement(a)) : a;
-	vector<int> b_abs = (sign_b == 1) ? increment(complement(b)) : b;
+	int sign_a = getSign(firstNumber);
+	int sign_b = getSign(secondNumber);
+	vector<int> a_abs = (sign_a == 1) ? increment(complement(firstNumber)) : firstNumber;
+	vector<int> b_abs = (sign_b == 1) ? increment(complement(secondNumber)) : secondNumber;
 	for (int i = 0; i < size; ++i) {
 		if (b_abs[i] == 1) {
 			vector<int> shiftedA = a_abs;
@@ -117,11 +128,10 @@ vector<int> multiplication(const vector<int>& a, const vector<int>& b) {
 	result.insert(result.begin(), result_sign);
 	return result;
 }
-vector<int> divide_binary(vector<int> dividend, vector<int> divisor, int precision) {
+vector<int> divideBinary(vector<int> dividend, vector<int> divisor, int precision) {
 	vector<int> quotient;
 	vector<int> remainder;
 	remainder = dividend;
-
 	for (int i = 0; i < precision; ++i) {
 		int carry = 0;
 		for (size_t j = 0; j < remainder.size(); ++j) {
@@ -131,7 +141,6 @@ vector<int> divide_binary(vector<int> dividend, vector<int> divisor, int precisi
 		}
 		quotient.push_back(carry);
 	}
-
 	return quotient;
 }
 void printBinary(const vector<int>& binary) {
@@ -140,55 +149,80 @@ void printBinary(const vector<int>& binary) {
 	}
 	cout << endl;
 }
-vector<int> fractionalBinary(double num) {
-	vector<int> binary;
-	bool isNegative = (num < 0);
-	if (isNegative)
-		num = -num;
-
-	binary.push_back(isNegative ? 1 : 0);
-
-	int integerPart = static_cast<int>(num);
-	double fractionalPart = num - integerPart;
-
-	while (integerPart > 0) {
-		binary.insert(binary.begin() + 1, integerPart % 2);
-		integerPart /= 2;
+void printBinaryy2(const vector<int>& binary) {
+	for (int bit : binary) {
+		cout << bit;
 	}
-
-	binary.push_back(-1);
-
-	int precision = 32;
-	while (fractionalPart > 0 && precision--) {
-		fractionalPart *= 2;
-		binary.push_back(static_cast<int>(fractionalPart));
-		fractionalPart -= static_cast<int>(fractionalPart);
-	}
-
-	return binary;
 }
-
-
-vector<int> sumOfBinaryNumbers(const vector<double>& numbers) {
-	vector<int> sum(32, 0); 
-
-	for (double num : numbers) {
-		vector<int> binary = fractionalBinary(num);
-		int carry = 0;
-		for (int i = 31; i >= 0; --i) {
-			int bitSum = sum[i] + binary[i] + carry;
-			sum[i] = bitSum % 2;
-			carry = bitSum / 2;
-		}
+vector<int> addBinary(const vector<int>& aInt, const vector<int>& aFrac, const vector<int>& bInt, const vector<int>& bFrac, int aFracSize, int bFracSize) {
+	vector<int> result;
+	int carry = 0;
+	int maxSizeFrac = max(aFracSize, bFracSize);
+	vector<int> tempAFrac(maxSizeFrac, 0);
+	vector<int> tempBFrac(maxSizeFrac, 0);
+	for (int i = aFrac.size() - 1, j = maxSizeFrac - 1; i >= 0; --i, --j) {
+		tempAFrac[j] = aFrac[i];
 	}
+	for (int i = bFrac.size() - 1, j = maxSizeFrac - 1; i >= 0; --i, --j) {
+		tempBFrac[j] = bFrac[i];
+	}
+	for (int i = maxSizeFrac - 1; i >= 0; --i) {
+		int sum = tempAFrac[i] + tempBFrac[i] + carry;
+		result.insert(result.begin(), sum % 2);
+		carry = sum / 2;
+	}
+	int maxSizeInt = max(aInt.size(), bInt.size()) + 1; 
+	vector<int> tempAInt(maxSizeInt, 0);
+	vector<int> tempBInt(maxSizeInt, 0);
+	for (int i = aInt.size() - 1, j = maxSizeInt - 1; i >= 0; --i, --j) {
+		tempAInt[j] = aInt[i];
+	}
+	for (int i = bInt.size() - 1, j = maxSizeInt - 1; i >= 0; --i, --j) {
+		tempBInt[j] = bInt[i];
+	}
+	for (int i = maxSizeInt - 1; i >= 0; --i) {
+		int sum = tempAInt[i] + tempBInt[i] + carry;
+		result.insert(result.begin(), sum % 2);
+		carry = sum / 2;
+	}
+	if (carry) {
+		result.insert(result.begin(), carry);
+	}
+	int pointPos = aFracSize > bFracSize ? aFracSize : bFracSize;
+	result.insert(result.end() - pointPos, 1);
 
-	return sum;
+	return result;
+}
+pair<vector<int>, vector<int>> doubleToBinary(double number) {
+	int intPart = static_cast<int>(number);
+	double fracPart = number - intPart;
+	vector<int> intBinary;
+	vector<int> fracBinary;
+	while (intPart > 0) {
+		intBinary.insert(intBinary.begin(), intPart % 2);
+		intPart /= 2;
+	}
+	int precision = 3; 
+	for (int i = 0; i < precision; ++i) {
+		fracPart *= 2;
+		int digit = static_cast<int>(fracPart);
+		fracBinary.push_back(digit);
+		fracPart -= digit;
+	}
+	return make_pair(intBinary, fracBinary);
 }
 
 
 void printBinaryy(const vector<int>& binary) {
-	bool isFractionalPart = false; 
+	bool isFractionalPart = false;
 	int fractionDigits = 0;
+	int pointIndex = -1;
+	for (int i = 0; i < binary.size(); ++i) {
+		if (binary[i] == -1) {
+			pointIndex = i;
+			break;
+		}
+	}
 
 	for (int i = 0; i < binary.size(); ++i) {
 		if (binary[i] == -1) {
@@ -204,12 +238,9 @@ void printBinaryy(const vector<int>& binary) {
 			}
 		}
 	}
-
-
-	while (fractionDigits < 8) {
-		cout << "0";
-		fractionDigits++;
-	}
-
+		while (fractionDigits < 8) {
+			cout << "0";
+			fractionDigits++;
+		}
 	cout << endl;
 }
