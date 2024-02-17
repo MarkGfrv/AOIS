00488fc44d872d70966527cb9e608c7bed7c6c1b
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2024-02-17 21:21:39 +0300

    Update binaryNumbers.h

--- 1lab/binaryNumbers.h
+++ 1lab/binaryNumbers.h
@@ -6,17 +6,18 @@
 #include<algorithm>
 #include<string>
 using namespace std;
-vector<int> convert_straight(int Number);
-vector<int> convert_inverse(int Number);
-vector<int> convert_dop(int Number);
+vector<int> convertStraight(int Number);
+vector<int> convertInverse(int Number);
+vector<int> convertDop(int Number);
 vector<int> addition(const vector<int>& a, const vector<int>& b);
 vector<int> substraction(const vector<int>& a, const vector<int>& b);
 vector<int> complement(const vector<int>& a);
 vector<int> increment(const vector<int>& a);
 int getSign(const vector<int>& a);
 vector<int> multiplication(const vector<int>& a, const vector<int>& b);
-vector<int> divide_binary(vector<int> dividend, vector<int> divisor, int precision);
+vector<int> divideBinary(vector<int> dividend, vector<int> divisor, int precision);
 void printBinary(const vector<int>& binary);
-vector<int> fractionalBinary(double num);
-vector<int> sumOfBinaryNumbers(const vector<double>& numbers);
+vector<int> addBinary(const vector<int>& aInt, const vector<int>& aFrac, const vector<int>& bInt, const vector<int>& bFrac, int aFracSize, int bFracSize);
+pair<vector<int>, vector<int>> doubleToBinary(double number);
 void printBinaryy(const vector<int>& binary);
+void printBinaryy2(const vector<int>& binary);
