c7cfafbcc3e6d823d89894054ada29c6b78d9d46
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2024-02-17 21:24:05 +0300

    Update binaryTests.cpp

--- 1lab/binaryTests.cpp
+++ 1lab/binaryTests.cpp
@@ -11,22 +11,22 @@ namespace MyTests
 	public:
         TEST_METHOD(ConvertStraightTest)
         {
-            Assert::IsTrue(convert_straight(13) == vector<int>{0, 1, 1, 0, 1});
-            Assert::IsTrue(convert_straight(-13) == vector<int>{1, 1, 1, 0, 1});
-            Assert::IsTrue(convert_straight(21) == vector<int>{0, 1, 0, 1, 0, 1});
+            Assert::IsTrue(convertStraight(13) == vector<int>{0, 1, 1, 0, 1});
+            Assert::IsTrue(convertStraight(-13) == vector<int>{1, 1, 1, 0, 1});
+            Assert::IsTrue(convertStraight(21) == vector<int>{0, 1, 0, 1, 0, 1});
         }
 
         TEST_METHOD(ConvertInverseTest)
         {
-            Assert::IsTrue(convert_inverse(13) == vector<int>{0, 0, 0, 1, 0});
-            Assert::IsTrue(convert_inverse(-13) == vector<int>{1, 0, 0, 1, 0});
-            Assert::IsTrue(convert_inverse(21) == vector<int>{0, 0, 1, 0, 1, 0});
+            Assert::IsTrue(convertInverse(13) == vector<int>{0, 1, 1, 0, 1});
+            Assert::IsTrue(convertInverse(-13) == vector<int>{1, 0, 0, 1, 0});
+            Assert::IsTrue(convertInverse(21) == vector<int>{0, 1, 0, 1, 0, 1});
         }
         TEST_METHOD(ConvertDopTest)
         {
-            Assert::IsTrue(convert_dop(13) == vector<int>{1, 0, 0, 1, 1});
-            Assert::IsTrue(convert_dop(-13) == vector<int>{1, 0, 0, 1, 1});
-            Assert::IsTrue(convert_dop(21) == vector<int>{1, 0, 1, 0, 1, 1});
+            Assert::IsTrue(convertDop(13) == vector<int>{0, 1, 1, 0, 1});
+            Assert::IsTrue(convertDop(-13) == vector<int>{1, 0, 0, 1, 1});
+            Assert::IsTrue(convertDop(21) == vector<int>{0, 1, 0, 1, 0, 1});
         }
         TEST_METHOD(AdditionTest)
         {
@@ -43,15 +43,14 @@ namespace MyTests
         {
             Assert::IsTrue(increment({ 1, 1, 0 }) == vector<int>{1, 1, 1});
             Assert::IsTrue(increment({ 1, 1, 1 }) == vector<int>{0, 0, 0});
-        }
-        TEST_METHOD(MultiplicationTest)
+        }        TEST_METHOD(MultiplicationTest)
         {
             Assert::IsTrue(multiplication({ 0, 1, 1, 0, 1 }, { 1, 1, 0, 1, 0, 1 }) == vector<int>{1, 1, 1, 0, 0, 0, 1, 0, 1, 0});
             Assert::IsTrue(multiplication({ 0, 1, 1, 0, 1 }, { 0, 1, 0, 1, 0, 1 }) == vector<int>{0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0});
         }
         TEST_METHOD(DivideBinaryTest)
         {
-            Assert::IsTrue(divide_binary({ 1, 0, 0, 1, 1 }, { 1, 0, 1, 0, 1, 1 }, 5) == std::vector<int>{1, 1, 0, 0, 1});
+            Assert::IsTrue(divideBinary({ 1, 0, 0, 1, 1 }, { 1, 0, 1, 0, 1, 1 }, 5) == std::vector<int>{1, 1, 0, 0, 1});
         }
         TEST_METHOD(PrintBinaryTest) {
             ostringstream buffer;
@@ -61,6 +60,26 @@ namespace MyTests
             cout.rdbuf(old);
             Assert::IsTrue(buffer.str() == "10101\n");
         }
+        TEST_METHOD(TestPrintBinary)
+        {
+            vector<int> binary1 = { 0, 1, 0, 1 };
+            stringstream buffer1;
+            cout.rdbuf(buffer1.rdbuf());
+            printBinaryy(binary1);
+            Assert::IsTrue("0101\n" != buffer1.str(), L"Binary printout should not match incorrect value.");
+
+            vector<int> binary2 = { 1, 0, 1, 1, -1, 0, 1 };
+            stringstream buffer2;
+            cout.rdbuf(buffer2.rdbuf());
+            printBinaryy(binary2);
+            Assert::IsTrue("1011.01\n" != buffer2.str(), L"Binary printout should not match incorrect value.");
+
+            vector<int> binary3 = { 0, 0, 1, 0, 0, -1, 1, 0 };
+            stringstream buffer3;
+            cout.rdbuf(buffer3.rdbuf());
+            printBinaryy(binary3);
+            Assert::IsTrue("0010.01\n" != buffer3.str(), L"Binary printout should not match incorrect value.");
+        }
+
 	};
 }
-
