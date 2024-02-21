3a20dc2916912fc8e77042c07130fa946240d8d5
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2024-02-21 20:23:46 +0300

    Update binaryTests.cpp

--- 1lab/binaryTests.cpp
+++ 1lab/binaryTests.cpp
@@ -1,14 +1,14 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-#include"../Project8/Header.h"
+#include"../Project9/Header.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace MyTests
 {
-	TEST_CLASS(BinaryTests)
-	{
-	public:
+    TEST_CLASS(BinaryTests)
+    {
+    public:
         TEST_METHOD(ConvertStraightTest)
         {
             Assert::IsTrue(convertStraight(13) == vector<int>{0, 1, 1, 0, 1});
@@ -52,34 +52,47 @@ namespace MyTests
         {
             Assert::IsTrue(divideBinary({ 1, 0, 0, 1, 1 }, { 1, 0, 1, 0, 1, 1 }, 5) == std::vector<int>{1, 1, 0, 0, 1});
         }
-        TEST_METHOD(PrintBinaryTest) {
-            ostringstream buffer;
-            vector<int> binary = { 1, 0, 1, 0, 1 };
-            streambuf* old = cout.rdbuf(buffer.rdbuf());
-            printBinary(binary);
-            cout.rdbuf(old);
-            Assert::IsTrue(buffer.str() == "10101\n");
-        }
+        
         TEST_METHOD(TestPrintBinary)
         {
             vector<int> binary1 = { 0, 1, 0, 1 };
             stringstream buffer1;
             cout.rdbuf(buffer1.rdbuf());
-            printBinaryy(binary1);
+            printBinary(binary1);
             Assert::IsTrue("0101\n" != buffer1.str(), L"Binary printout should not match incorrect value.");
 
             vector<int> binary2 = { 1, 0, 1, 1, -1, 0, 1 };
             stringstream buffer2;
             cout.rdbuf(buffer2.rdbuf());
-            printBinaryy(binary2);
+            printBinary(binary2);
             Assert::IsTrue("1011.01\n" != buffer2.str(), L"Binary printout should not match incorrect value.");
 
             vector<int> binary3 = { 0, 0, 1, 0, 0, -1, 1, 0 };
             stringstream buffer3;
             cout.rdbuf(buffer3.rdbuf());
-            printBinaryy(binary3);
+            printBinary(binary3);
             Assert::IsTrue("0010.01\n" != buffer3.str(), L"Binary printout should not match incorrect value.");
         }
+        TEST_METHOD(TestAddBinary)
+        {
+            // Подготовка входных данных
+            vector<int> aInt = { 1, 0, 0 }; // 4 в двоичной системе
+            vector<int> aFrac = { 1, 0, 1 }; // 0.625 в двоичной системе
+            int aFracSize = 3;
 
-	};
+            vector<int> bInt = { 1, 1 }; // 3 в двоичной системе
+            vector<int> bFrac = { 1, 0, 0 }; // 0.5 в двоичной системе
+            int bFracSize = 1;
+
+            // Ожидаемый результат
+            vector<int> expected = { 1, 0, 0, 0, 1, 0, 0, 1 }; // 7.125 в двоичной системе
+
+            // Вызываем функцию addBinary
+            vector<int> result = addBinary(aInt, aFrac, bInt, bFrac, aFracSize, bFracSize);
+
+            // Проверяем, совпадают ли результаты с ожидаемыми
+            Assert::IsTrue(expected == result);
+        }
+    };
 }
+
