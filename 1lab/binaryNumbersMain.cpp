0982298461a957499fdaf4365ddaa54ff55814ef
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2024-02-21 20:23:17 +0300

    Update binaryNumbersMain.cpp

--- 1lab/binaryNumbersMain.cpp
+++ 1lab/binaryNumbersMain.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include"../Project8/Header.h"
+#include"../Project9/Header.h"
 int main() {
 	setlocale(LC_ALL, "Russian");
 	int choice;
@@ -10,42 +10,54 @@ int main() {
 	vector<int> BinaryNumber = convertStraight(Numberr1);
 	cout << "Двоичное представления числа " << Numberr1 << " прямым кодом:" << endl;
 	printBinary(BinaryNumber);
+	cout << endl;
 	vector<int> BinaryNumber2 = convertInverse(Numberr1);
 	cout << "Двоичное представления числа " << Numberr1 << " обратным кодом:" << endl;
 	printBinary(BinaryNumber2);
+	cout << endl;
 	vector<int> BinaryNumber3 = convertDop(Numberr1);
 	cout << "Двоичное представления числа " << Numberr1 << " дополнительным кодом:" << endl;
 	printBinary(BinaryNumber3);
+	cout << endl;
 
 	vector<int> BinaryNumber4 = convertStraight(Numberr2);
 	cout << "Двоичное представления числа " << Numberr2 << " прямым кодом:" << endl;
 	printBinary(BinaryNumber4);
+	cout << endl;
 	vector<int> BinaryNumber5 = convertInverse(Numberr2);
 	cout << "Двоичное представления числа " << Numberr2 << " обратным кодом:" << endl;
 	printBinary(BinaryNumber5);
+	cout << endl;
 	vector<int> BinaryNumber6 = convertDop(Numberr2);
 	cout << "Двоичное представления числа " << Numberr2 << " дополнительным кодом:" << endl;
 	printBinary(BinaryNumber6);
+	cout << endl;
 
 	vector<int> BinaryNumber7 = convertStraight(Numberr3);
 	cout << "Двоичное представления числа " << Numberr3 << " прямым кодом:" << endl;
 	printBinary(BinaryNumber7);
+	cout << endl;
 	vector<int> BinaryNumber8 = convertInverse(Numberr3);
 	cout << "Двоичное представления числа " << Numberr3 << " обратным кодом:" << endl;
 	printBinary(BinaryNumber8);
+	cout << endl;
 	vector<int> BinaryNumber9 = convertDop(Numberr3);
 	cout << "Двоичное представления числа " << Numberr3 << " дополнительным кодом:" << endl;
 	printBinary(BinaryNumber9);
+	cout << endl;
 
 	vector<int> BinaryNumber10 = convertStraight(Numberr4);
 	cout << "Двоичное представления числа " << Numberr4 << " прямым кодом:" << endl;
 	printBinary(BinaryNumber10);
+	cout << endl;
 	vector<int> BinaryNumber11 = convertInverse(Numberr4);
 	cout << "Двоичное представления числа " << Numberr4 << " обратным кодом:" << endl;
 	printBinary(BinaryNumber11);
+	cout << endl;
 	vector<int> BinaryNumber12 = convertDop(Numberr4);
 	cout << "Двоичное представления числа " << Numberr4 << " дополнительным кодом:" << endl;
 	printBinary(BinaryNumber12);
+	cout << endl;
 
 	vector<int> sum1 = addition(BinaryNumber3, BinaryNumber6);
 	vector<int> sum2 = addition(BinaryNumber3, BinaryNumber12);
@@ -68,13 +80,13 @@ int main() {
 	vector<int> quotient_binary2 = divideBinary(BinaryNumber3, BinaryNumber12, precision);
 	vector<int> quotient_binary3 = divideBinary(BinaryNumber7, BinaryNumber4, precision);
 	vector<int> quotient_binary4 = divideBinary(BinaryNumber7, BinaryNumber10, precision);
-	double num1 = 13.100;
-	double num2 = 21.101;
+	double num1 = 4.625;
+	double num2 = 3.5;
 	pair<vector<int>, vector<int>> binaryy1 = doubleToBinary(num1);
 	pair<vector<int>, vector<int>> binaryy2 = doubleToBinary(num2);
 	vector<int> summ1 = addBinary(binaryy1.first, binaryy1.second, binaryy2.first, binaryy2.second, binaryy1.second.size(), binaryy2.second.size());
 	int pointPos1 = binaryy1.second.size() > binaryy2.second.size() ? binaryy1.second.size() : binaryy2.second.size();
-	int pos1 = summ1.size() - pointPos1 - 2;
+	int pos1 = summ1.size() - pointPos1 - 1;
 
 	do {
 		std::cout << "1. Сумма чисел\n"
@@ -88,49 +100,61 @@ int main() {
 
 		switch (choice) {
 		case 1:
-			cout << "Сумма в двоичной системе счисления(дополнительный код) чисел " << Numberr1 << " и " << Numberr2 << " равна: " << endl;
+			cout << "Сумма в двоичной системе счисления(дополнительный код) " << Numberr1 << " и " << Numberr2 << " равна: " << endl;
 			printBinary(sum1);
+			cout << endl;
 
 			cout << "Сумма в двоичной системе счисления(дополнительный код) чисел " << Numberr1 << " и " << Numberr4 << " равна: " << endl;
 			printBinary(sum2);
+			cout << endl;
 
 			cout << "Сумма в двоичной системе счисления(дополнительный код) чисел " << Numberr3 << " и " << Numberr2 << " равна: " << endl;
 			printBinary(sum3);
+			cout << endl;
 
 			cout << "Сумма в двоичной системе счисления(дополнительный код) чисел " << Numberr3 << " и " << Numberr4 << " равна: " << endl;
 			printBinary(sum4);
+			cout << endl;
 			break;
 
 		case 2:
 			cout << "Разность в двоичной системе счисления(дополнительный код) чисел " << Numberr1 << " и " << Numberr2 << " равна: " << endl;
 			printBinary(diff1);
+			cout << endl;
 
 			cout << "Разность в двоичной системе счисления(дополнительный код) чисел " << Numberr1 << " и " << Numberr4 << " равна: " << endl;
 			printBinary(diff2);
+			cout << endl;
 
 			cout << "Разность в двоичной системе счисления(дополнительный код) чисел " << Numberr3 << " и " << Numberr2 << " равна: " << endl;
 			printBinary(diff3);
+			cout << endl;
 
 			cout << "Разность в двоичной системе счисления(дополнительный код) чисел " << Numberr3 << " и " << Numberr4 << " равна: " << endl;
 			printBinary(diff4);
+			cout << endl;
 			break;
 
 		case 3:
 			cout << "Произведение в двоичной системе счисления(прямой код) чисел " << Numberr1 << " и " << Numberr2 << " равна: " << endl;
 			printBinary(mult);
+			cout << endl;
 			cout << "Знак произведения: " << product_sign << endl;
 
 			cout << "Произведение в двоичной системе счисления(прямой код) чисел " << Numberr1 << " и " << Numberr4 << " равна: " << endl;
 			printBinary(mult4);
+			cout << endl;
 			cout << "Знак произведения: " << product_sign4 << endl;
 
 
 			cout << "Произведение в двоичной системе счисления(прямой код) чисел " << Numberr3 << " и " << Numberr2 << " равна: " << endl;
 			printBinary(mult7);
+			cout << endl;
 			cout << "Знак произведения: " << product_sign7 << endl;
 
 			cout << "Произведение в двоичной системе счисления(прямой код) чисел " << Numberr3 << " и " << Numberr4 << " равна: " << endl;
 			printBinary(mult10);
+			cout << endl;
 			cout << "Знак произведения: " << product_sign10 << endl;
 			break;
 
@@ -168,14 +192,14 @@ int main() {
 
 		case 5:
 
-			cout << "Числа " << num1 << " ; " << num2 <<  " в двоичной системе : \n";
-			printBinaryy2(binaryy1.first);
+			cout << "Числа " << num1 << " ; " << num2 << " в двоичной системе : \n";
+			printBinary(binaryy1.first);
 			cout << ".";
-			printBinaryy2(binaryy1.second);
+			printBinary(binaryy1.second);
 			cout << endl;
-			printBinaryy2(binaryy2.first);
+			printBinary(binaryy2.first);
 			cout << ".";
-			printBinaryy2(binaryy2.second);
+			printBinary(binaryy2.second);
 			cout << endl;
 
 			cout << "Сумма чисел " << num1 << " и " << num2 << " в двоичной системе: " << endl;
