a9d1f74df2fe349dd3f4d3bd53fd62fd68872c75
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2024-02-24 17:44:20 +0300

    Update binaryNumbers.h

--- 1lab/binaryNumbers.h
+++ 1lab/binaryNumbers.h
@@ -17,5 +17,5 @@ int getSign(const vector<int>& a);
 vector<int> multiplication(const vector<int>& a, const vector<int>& b);
 vector<int> divideBinary(vector<int> dividend, vector<int> divisor, int precision);
 void printBinary(const vector<int>& binary);
-vector<int> addBinary(const vector<int>& aInt, const vector<int>& aFrac, const vector<int>& bInt, const vector<int>& bFrac, int aFracSize, int bFracSize);
-pair<vector<int>, vector<int>> doubleToBinary(double number);
+vector<int> addBinary(vector<int> num1, vector<int> num2);
+vector<int> doubleToBinary(double num);
