5f6254ef7581391341076116ca8f3b8a74ab1959
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2024-03-16 10:41:07 +0300

    Update LogicalExxpresion.h

--- lab2/LogicalExxpresion.h
+++ lab2/LogicalExxpresion.h
@@ -8,6 +8,15 @@
 
 using namespace std;
 
+void negationn(stack<bool>& operandStack);
+void con(stack<bool>& operandStack);
+void dis(stack<bool>& operandStack);
+void implic(stack<bool>& operandStack);
+void equiv(stack<bool>& operandStack);
 bool evaluateLogicalExpression(const vector<bool>& variables, const string& expression);
 int countVariables(const string& expression);
-void solveExpression(const string& expression);
+void solveLogicalExpression(const string& expression);
+void printVariables(int numVariables);
+void printTruthTable(const vector<bool>& variables, bool result);
+void calculateSknfAndSdnf(const vector<bool>& variables, bool result, int rowNumber, const int numVariables, vector<string>& sknfClauses,
+vector<string>& sdnfClauses, vector<int>& sknfNumbers, vector<int>& sdnfNumbers);
